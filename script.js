1ddcb4e8c1a995754f42a6093bea74ee9f8fbaa7
Adelson Barros Dos Santos <40273131+adelson101@users.noreply.github.com>
2023-12-28 16:08:20 -0300

    update

--- script.js
+++ script.js
@@ -15,52 +15,44 @@ function carregar(){
 }
 
 function verificar(){
+
     const data = new Date();
     const idade = data.getFullYear()-Number(document.getElementById('iNascimento').value);
+    var genero = '';
 
-    if (document.getElementsByName('sexo')[0].checked && Number(document.getElementById('iNascimento').value)!=0)
-        {
-            document.getElementById('resultado').style.display = 'block';
-            document.getElementById('resultado-txt').innerHTML = `Detectado Homem que tem ${idade} Anos.`;
-            if(idade>=50){
-                document.getElementById('resultado').style.backgroundImage = "url('img/idoso.png')";   
-            }
-            else if(idade>=18){
-                document.getElementById('resultado').style.backgroundImage = "url('img/homem.png')";   
-            }
-            else if(idade>=0){
-                document.getElementById('resultado').style.backgroundImage = "url('img/garoto.png')";   
-            }
-            else
-            {
-                window.alert('Verificar se adicionou uma idade correta');
-                document.getElementById('resultado-txt').innerHTML = `Valor Invalido!`;
-                document.getElementById('resultado').style.display = 'none';
-            }
-        }
+    if( document.getElementById('iNascimento').value.length == 0 || document.getElementById('iNascimento').value > data.getFullYear() ) {
+        window.alert('[ERROR] valor invalido');
+    } else {
+        
+        document.getElementById('resultado').style.display = 'block';
 
-        else if (document.getElementsByName('sexo')[1].checked && Number(document.getElementById('iNascimento').value)!=0){
-            document.getElementById('resultado').style.display = 'block';
-            document.getElementById('resultado-txt').innerHTML = `Detectado Mulher que tem ${idade} Anos.`;
-            if(idade>=50){
-                document.getElementById('resultado').style.backgroundImage = "url('img/idosa.png')";   
-            }
-            else if(idade>=18){
-                document.getElementById('resultado').style.backgroundImage = "url('img/mulher.png')";   
-            }
-            else if(idade>=0){
-                document.getElementById('resultado').style.backgroundImage = "url('img/garota.png')";   
-            }
-            else
-            {
-                window.alert('Verificar se adicionou uma idade correta');
-                document.getElementById('resultado-txt').innerHTML = `Valor Invalido!`;
-                document.getElementById('resultado').style.display = 'none';
-            }
-            
+        if (document.getElementsByName('sexo')[0].checked) {
+                genero = 'Homem';
+
+                if(idade>=50){
+                    document.getElementById('resultado').style.backgroundImage = "url('img/idoso.png')";   
+                }
+                else if(idade>=18){
+                    document.getElementById('resultado').style.backgroundImage = "url('img/homem.png')";   
+                }
+                else if(idade>=0){
+                    document.getElementById('resultado').style.backgroundImage = "url('img/garoto.png')";   
+                }
+        } else if ( document.getElementsByName('sexo')[1].checked ) {
+                genero = 'Mulher';
+
+                if(idade>=50){
+                    document.getElementById('resultado').style.backgroundImage = "url('img/idosa.png')";   
+                }
+                else if(idade>=18){
+                    document.getElementById('resultado').style.backgroundImage = "url('img/mulher.png')";   
+                }
+                else if(idade>=0){
+                    document.getElementById('resultado').style.backgroundImage = "url('img/garota.png')";   
+                }
         }
-        else {
-            window.alert('Digite os dados.')
-        }
+        document.getElementById('resultado-txt').innerHTML = `Detectado ${genero} que tem ${idade} Anos.`;
+
+    }
 
     }

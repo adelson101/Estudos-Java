5f0c6338ab10afd5ce317ad0fc5a6cf03313feeb
Adelson Barros Dos Santos <40273131+adelson101@users.noreply.github.com>
2023-12-29 18:15:45 -0300

    update

--- ex0001.js
+++ ex0001.js
@@ -1,53 +1,72 @@
 function verificar() {
     
     let cont =  document.getElementById('Contagem');
-    let diferente = true;
+
+    // Valida se todos os campos foram preenchediso
 
     if ( document.getElementById('iinicio').value.length==0 || document.getElementById('ifinal').value.length==0 || document.getElementById('ipasso').value.length==0 )
     {
         cont.innerHTML = '[ERROR] Falta valores';
     }
+
+    // Final da Validação
     
     else 
     
     {   
         
-        do {
-
-        if(Number(document.getElementById('ipasso').value)>0){
-            
-            cont.innerHTML = 'Contando:<br>'
-
-            for (let i = Number(document.getElementById('iinicio').value) ; i <= Number(document.getElementById('ifinal').value) ; i=i+Number(document.getElementById('ipasso').value)) 
-            {
-                cont.innerHTML += `<span>👉 ${i}</span>`;
-            }
-            cont.innerHTML += `<span>👉 🚩</span>`;
-
-            diferente = false;
-
-        }
-        else 
+        if(document.getElementById('ipasso').value <= 0) //Verifica se os passos são maiores que 0
         {
+
             window.alert('Invalido Conta apartir do PASSO 1');
             document.getElementById('ipasso').value = 1;
-            diferente = true;
+
         }
 
-        } while(diferente)
+            cont.innerHTML = 'Contando:<br>'
+
+            if ( Number(document.getElementById('iinicio').value) < Number(document.getElementById('ifinal').value ) ) {
+ 
+                //Contagem Crescente
+                for (let i = Number(document.getElementById('iinicio').value) ; i <= Number(document.getElementById('ifinal').value) ; i += Number(document.getElementById('ipasso').value)) 
+                {
+                    cont.innerHTML += `<span>👉 ${i}</span>`;
+                }
+
+            }else {
+
+                //Contagem Regressiva
+                for (let i = Number(document.getElementById('iinicio').value) ; i >= Number(document.getElementById('ifinal').value) ; i -= Number(document.getElementById('ipasso').value )) 
+                {
+                    cont.innerHTML += `<span>👉 ${i}</span>`;
+                }
+
+            }
+            
+            cont.innerHTML += `<span>👉 🚩</span>`;
 
     }
 }
 
 function tabuada(){
-    if (document.getElementById('inumero').value.length==0){
+
+    if (document.getElementById('inumero').value.length==0)
+
+    {
+
        window.alert('Digite um numero');
-    } else {
+
+    }
+
+    else 
+    {
+
         document.getElementById('icaixa').innerHTML = '';
-        for (var i = 1 ; i <= 10 ; i++){
+        
+        for (var i = 1 ; i <= 10 ; i++)
+        {
             document.getElementById('icaixa').innerHTML += `<p>${document.getElementById('inumero').value } X ${i} = ${document.getElementById('inumero').value*i}</p>`
         }
     }
-    
     
 }
